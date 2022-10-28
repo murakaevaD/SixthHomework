6430b1ff7e7b592a1ed88523e07e6ef6ddad7e79
murakaevaD <daniamurakaeva@yandex.ru>
2022-10-28 21:29:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Na_29._10
+{
+    public class hospital
+    {
+        string[] Insurances = new string[4];
+        string[] TypeOfIllness = new string[4];
+        string AcceptMessage;
+        string FailedMesssage;
+        string both;
+        string Insurance1;
+
+        int Price;
+        public hospital()
+        {
+            Insurances = new[] { "Легкое заболевание", "Телесные повреждения", "Утрата трудоспособности", "Авария", "Онкология" };
+            Price = 200;
+            TypeOfIllness = new[] { "ветрянка", "простуда обыкновенная", "инфаркт", "коронавирус", "воспаление легких" };
+            AcceptMessage = "Пациент принят";
+            FailedMesssage = "Пациент не принят,обратитесь в другую больницу";
+
+            both = FailedMesssage;
+        }
+
+        public void setSearchIll(string Ill)
+        {
+
+            for (int i = 0; i < 4; i++)
+                if (Ill == TypeOfIllness[i])
+                {
+                    both = AcceptMessage;
+
+                    break;
+
+                }
+        }
+
+        public string getSearchIll()
+        {
+            return both;
+        }
+
+        public void setSearchInsurance(string Insurance2)
+        {
+            string Insurance = "Страховка подходит";
+            Insurance1 = "Страховка не подходит";
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (Insurance2 == Insurances[i])
+                {
+                    Insurance1 = Insurance;
+                    continue;
+                }
+            }
+        }
+        public string getSearchInsurance()
+        {
+            return Insurance1;
+        }
+        public int Pricсee()
+        {
+            return Price;
+        }
+    }
+        public class doctor : hospital
+    {
+        string[,] Doctors = new string[5, 4];
+        string DoctorName;
+        string DoctorSpeciality;
+        string DoctorDay;
+        long DoctorPhone;
+        string DoctorID;
+        public doctor()
+        {
+            Doctors = new[,] 
+            {
+                {"Люкшин А.Ю.","инфекционист","09122222229","00001","восресенье"},
+                {"Миронов Ф.В.","терапевт","09121122339","00002","понедельник"},
+                {"Фейзуллина Л.О","невролог","09364455123","00003","среда"},
+                {"Минзянов Р.Л.","вирусолог","09394589631","00004","пятница"},
+                {"Листопадова А.В.","лор","09360000001","00005","суббота"}
+            };
+
+        }
+        public void setDoctorSpeciality(string Illness)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (Illness == Doctors[j, 1])
+                {
+                    DoctorName = Doctors[j, 0];
+                    DoctorSpeciality = Doctors[j, 1];
+                    DoctorPhone = long.Parse(Doctors[j, 2]);
+                    DoctorID = Doctors[j, 3];
+                    DoctorDay = Doctors[j, 4];
+                    break;
+                }
+                else
+                    continue;
+            }
+        }
+        public string getDoctorName()
+        {
+            return DoctorName;
+        }
+        public string getDoctorSpeciality()
+        {
+            return DoctorSpeciality;
+        }
+        public long getDoctorPhone()
+        {
+            return DoctorPhone;
+        }
+        public string getDoctorID()
+        {
+            return DoctorID;
+        }
+        public string getDoctorRecept()
+        {
+            return DoctorDay;
+        }
+
+    }
+    public class pationt : doctor
+    {
+        Random PationtID1;
+
+        string HistoryOfDisease;
+        string RecentDisease;
+        string TypeOfInsurance;
+        string Name;
+        long PhoneNumber;
+        int PationtID;
+        int cost;
+        int Age;
+
+        string generation1;
+
+        public void setGeneration(bool Generation)
+        {
+            if (Generation == true)
+                generation1 = "Жен";
+            else
+                generation1 = "Муж";
+        }
+
+        public string getGeneration()
+        {
+            return generation1;
+        }
+
+        public void setHistoryOfDisease(string HistoryOfDisease)
+        {
+
+            this.HistoryOfDisease = HistoryOfDisease;
+        }
+
+        public string getHistoryOfDisease()
+        {
+            return HistoryOfDisease;
+        }
+
+        public void setRecentDisease(string RecentDisease)
+        {
+            this.RecentDisease = RecentDisease;
+        }
+
+        public string getRecentDisease()
+        {
+            return RecentDisease;
+        }
+
+        public void setTypeOfInsurenc(string insurenc)
+        {
+            TypeOfInsurance = insurenc;
+        }
+
+        public string getTypeOfInsurenc()
+        {
+            return TypeOfInsurance;
+        }
+
+        public void setName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public void setPhoneNumber(long PhoneNumber)
+        {
+            this.PhoneNumber = PhoneNumber;
+        }
+
+        public long getPhoneNumber()
+        {
+            return PhoneNumber;
+        }
+
+        public void setPationtID()
+        {
+            PationtID1 = new Random();
+            PationtID = PationtID1.Next(10000, 20000);
+
+        }
+        public int getPationtID()
+        {
+            return PationtID;
+        }
+        public void setCuringDay(int cost, string InsuranceResualt)
+        {
+            if (InsuranceResualt == "Страховка не подходит")
+            {
+                this.cost = Pricсee() * cost;
+            }
+            else
+            {
+                this.cost = (Pricсee() * cost * 50 / 100);
+            }
+        }
+
+        public int getCuringDay()
+        {
+            return cost;
+        }
+        public void setAge(int Age)
+        {
+            this.Age = Age;
+        }
+
+        public int getAge()
+        {
+            return Age;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Diease;
+            string RecentDiease;
+            string Insurance;
+            string Name;
+            long PhoneNumber;
+            int Generation;
+            int cost;
+            int Age;
+            int pationts;
+            int i;
+
+            Console.WriteLine("Добро пожаловать в больницу №56");
+
+            Console.Write("Сколько пациентов хотите записать на прием : ");
+            pationts = int.Parse(Console.ReadLine());
+            string[] Insurances = new string[pationts];
+            pationt[] Pationt = new pationt[pationts];
+            hospital[] Hospital = new hospital[pationts];
+            doctor[] Doctor = new doctor[pationts];
+            for (i = 0; i < pationts; i++)
+            {
+                if (i > pationts - 1)
+                    break;
+
+                Pationt[i] = new pationt();
+                Hospital[i] = new hospital();
+                Doctor[i] = new doctor();
+
+                Console.WriteLine("Пациент номер " + (i + 1));
+
+                Console.WriteLine("--------");
+                Console.Write("Введите имя : ");
+                Name = Console.ReadLine();
+                Pationt[i].setName(Name);
+
+                Console.Write("Введите пол : ");
+                Console.Write("       1.Муж   ");
+                Console.WriteLine("2.Жен");
+                Generation = int.Parse(Console.ReadLine());
+                if (Generation == 1)
+                    Pationt[i].setGeneration(false);
+                else
+                    Pationt[i].setGeneration(true);
+
+                Console.Write("Введите возраст : ");
+                Age = int.Parse(Console.ReadLine());
+                Pationt[i].setAge(Age);
+
+                Console.Write("Введите номер телефона : ");
+                PhoneNumber = long.Parse(Console.ReadLine());
+                Pationt[i].setPhoneNumber(PhoneNumber);
+
+                Console.Write("Введите историю заболеваний : ");
+                Diease = Console.ReadLine();
+                Pationt[i].setHistoryOfDisease(Diease);
+
+                Console.Write("Что беспокоит на данный момент : ");
+                RecentDiease = Console.ReadLine();
+                Pationt[i].setRecentDisease(RecentDiease);
+
+                Doctor[i].setDoctorSpeciality(RecentDiease);
+
+                Hospital[i].setSearchIll(RecentDiease);
+                Console.WriteLine(Hospital[i].getSearchIll());
+
+                if (Hospital[i].getSearchIll() == "Пациент принят")
+                {
+
+                    Console.Write("Введите тип страховки : ");
+                    Insurance = Console.ReadLine();
+                    Pationt[i].setTypeOfInsurenc(Insurance);
+
+                    Hospital[i].setSearchInsurance(Insurance);
+                    Console.WriteLine(Hospital[i].getSearchInsurance());
+
+                    Insurances[i] = Hospital[i].getSearchInsurance();
+
+                    Console.WriteLine("-------");
+                    Pationt[i].setPationtID();
+                    Console.WriteLine("1.ID пациента : " + Pationt[i].getPationtID());
+                    Console.WriteLine("2.Имя: " + Pationt[i].getName());
+                    Console.WriteLine("3.Возраст: " + Pationt[i].getAge());
+                    Console.WriteLine("4.Номер телефона: " + Pationt[i].getPhoneNumber());
+                    Console.WriteLine("5.История болезней: " + Pationt[i].getHistoryOfDisease());
+                    Console.WriteLine("6.Заболевание на данный момент:" + Pationt[i].getRecentDisease());
+                    Console.WriteLine("7.Вид страховки: " + Pationt[i].getTypeOfInsurenc());
+                    Console.WriteLine("--------");
+                    Console.WriteLine("Информация о вашем докторе: ");
+                    Console.WriteLine("1.ФИО: " + Doctor[i].getDoctorName());
+                    Console.WriteLine("2.Специализация: " + Doctor[i].getDoctorSpeciality());
+                    Console.WriteLine("3.Номер телефона: " + Doctor[i].getDoctorPhone());
+                    Console.WriteLine("4.ID доктора: " + Doctor[i].getDoctorID());
+                    Console.WriteLine("5.День приема:" + Doctor[i].getDoctorRecept());
+                    Console.WriteLine("--------");
+                    cost = 200;
+                    Pationt[i].setCuringDay(cost, Insurances[i]);
+                    Console.WriteLine("Стоимость лечения: " + Pationt[i].getCuringDay() + " рублей");
+
+                    if (pationts == 1)
+                        break;
+                    if (pationts > 1)
+                        continue;
+
+                }
+            }
+            Console.WriteLine("Спасибо за обращение,до свидания!");
+        }
+    }
+}
---
+++ TymNa_29.10/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TymNa_29._10
+{
+    enum Bank
+    {
+        Текущий,
+        Cберегательный
+    }
+    class Bankk
+    {
+        private int numb;
+        private int numb2;
+        private int balance;
+        private Bank name;
+
+        public void Name()
+        {
+            Console.WriteLine("С каким счетом хотите работать: " + '\n' + "сберегательный или текущий ");
+            string name1 = Console.ReadLine();
+            if (name1 == "сберегательный")
+            {
+                Console.WriteLine("Вы выбрали " + Bank.Cберегательный + " счёт");
+            }
+            else if (name1 == "текущий")
+            {
+                Console.WriteLine("Вы выбрали " + Bank.Текущий + " счёт");
+            }
+            else
+            {
+                Console.WriteLine("Данные введены неверно");
+            }
+        }
+        public void Numb()
+        {
+            Console.WriteLine("Введите номер счета : ");
+            numb = int.Parse(Console.ReadLine());
+            Console.WriteLine("Номер вашего счета : " + numb);
+        }
+        public void Numb2()
+        {
+            Random random = new Random();
+            numb2 = random.Next();
+            Console.WriteLine("Номер вашего счета: " + numb2);
+        }
+        public void Balance()
+        {
+            Console.WriteLine("Выберите дальнейшее действие: " + '\n' + "узнать баланс или пополнить баланс");
+            string what = Console.ReadLine();
+            if (what == "узнать баланс")
+            {
+                int balance1 = 20000;
+                Console.WriteLine("Ваш баланс : " + balance1);
+            }
+
+            if (what == "пополнить баланс")
+            {
+                Console.Write("Введите сумму ,которую хотели бы внести на счет: ");
+                int sum = int.Parse(Console.ReadLine());
+                int balance2 = 20000 + sum;
+                Console.WriteLine("Вы внесли " + sum + " на счет. Ваш баланс сейчас составляет: " + balance2);
+            }
+        }
+        public void Th()
+        {
+            Console.WriteLine("Какую сумму хотите снять?: ");
+            int s = int.Parse(Console.ReadLine());
+            int balanc = 20000;
+            if (balanc < s)
+            {
+                Console.WriteLine("Недостаточно средств на счете");
+                Balance();
+
+            }
+            else
+            {
+                Console.WriteLine("После снятия средств на балансе осталось : " + (balanc - s));
+            }
+        }
+    }
+    class House
+    {
+        private int height;
+        private int floors;
+        private int flats;
+        private int entrances;
+        private int number;
+        public House(int height, int floors, int flats, int entrances)
+        {
+            this.height = height;
+            this.floors = floors;
+            this.flats = flats;
+            this.entrances = entrances;
+        }
+        public void Number()
+        {
+            Random rnd = new Random();
+            number = rnd.Next();
+            Console.WriteLine("Уникальный номер дома:" + number);
+        }
+        public void HeF()
+        {
+            Console.WriteLine("Высота одного этажа равна: " + height / floors);
+        }
+        public void Fl()
+        {
+            Console.WriteLine("Количество квартир в подъезде: " + flats / entrances);
+        }
+        public void fl()
+        {
+            Console.WriteLine("Количество квартир на этаже: " + flats / floors);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 7.1");
+            Bankk bank = new Bankk();
+            bank.Name();
+            bank.Numb();
+            bank.Balance();
+
+            Console.WriteLine("Упражнение 7.2");
+            Bankk bank1 = new Bankk();
+            bank1.Name();
+            bank1.Numb2();
+            bank1.Balance();
+
+            Console.WriteLine("Упражнение 7.3");
+            Bankk bank2 = new Bankk();
+            bank2.Name();
+            bank2.Numb2();
+            bank2.Th();
+
+            Console.WriteLine("ДЗ 7.1");
+            Console.Write("Введите высоту дома: ");
+            int height = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество этажей во всем доме: ");
+            int floors = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество квартир во всем доме: ");
+            int flats = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество подъездов во всем доме: ");
+            int entrances = int.Parse(Console.ReadLine());
+            House build = new House(height, floors, flats, entrances);
+            build.Number();
+            build.HeF();
+            build.Fl();
+            build.fl();
+        }
+    }
+}
